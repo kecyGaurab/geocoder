bd8142e2aaf906e72731a4e8e2431be198d8b7bb
kecyGaurab <kecy.gaurab@gmail.com>
2023-11-10 09:05:43 +0200

    feat: add global index

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -6,9 +6,27 @@ export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new dynamodb.Table(this, 'AddressTable', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Adjust based on your needs
+    // Define the DynamoDB table
+    const dynamoTable = new dynamodb.Table(this, 'AddressTable', {
+      partitionKey: {
+        name: 'addressId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, 
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+    });
+
+    // Add a Global Secondary Index (GSI) for coordinates
+    dynamoTable.addGlobalSecondaryIndex({
+      indexName: 'CoordinatesIndex',
+      partitionKey: {
+        name: 'latitude',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      sortKey: {
+        name: 'longitude',
+        type: dynamodb.AttributeType.NUMBER,
+      },
     });
   }
 }
